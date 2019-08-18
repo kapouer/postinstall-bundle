0dff733e9f4d409f55b0b07ddec87aeceb99f18d
Jérémy Lal <kapouer@melix.org>
2019-08-19 00:46:02 +0200

    Use stream.pipeline to make sure errors are forwarded

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const browserify = require("browserify");
 const fs = require('fs');
+const stream = require('stream');
 
 module.exports = function(inputs, output, options) {
 	if (inputs.length == 0) return Promise.resolve();
 
 	return new Promise(function(resolve, reject) {
 		var out = fs.createWriteStream(output);
-		out.on('finish', resolve);
-		out.on('error', reject);
-		browserify(inputs, options).bundle().pipe(out);
+		out.on("finish", resolve);
+		out.on("error", reject);
+		stream.pipeline(browserify(inputs, options).bundle(), out);
 	});
 };
 
