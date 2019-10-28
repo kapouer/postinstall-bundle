c404533330ef2042c0ed64284a5d141b642e5271
Jérémy Lal <kapouer@melix.org>
2019-10-29 00:12:09 +0100

    Use postinstall-cache

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const browserify = require("browserify");
-const fs = require('fs');
-const stream = require('stream');
+const Cache = require('postinstall-cache');
 
-module.exports = function(inputs, output, options) {
-	if (inputs.length == 0) return Promise.resolve();
-
-	return new Promise(function(resolve, reject) {
-		var out = fs.createWriteStream(output);
-		out.on("finish", resolve);
-		out.on("error", reject);
-		stream.pipeline(browserify(inputs, options).bundle(), out);
-	});
-};
+module.exports = Cache.worker({
+	dirname: __dirname
+});
 
---
+++ worker.js
@@ -0,0 +1,17 @@
+const browserify = require('browserify');
+const PassThrough = require('stream').PassThrough;
+const Path = require('path');
+
+module.exports = function(input, data, output, opts) {
+	opts = Object.assign({}, opts, {
+		basedir: Path.dirname(input)
+	});
+	var ps = new PassThrough();
+	ps.end(data);
+	return new Promise(function(resolve, reject) {
+		browserify(ps, opts).bundle(function(err, buf) {
+			if (err) reject(err);
+			else resolve(buf);
+		});
+	});
+};
